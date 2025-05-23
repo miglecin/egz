c0a32665c391960d03302363d16f2a31dec0e3d0
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-05-23 15:24:48 +0300

    update

---
+++ include/cross_reference.h
@@ -0,0 +1,9 @@
+#ifndef CROSS_REFERENCE_H
+#define CROSS_REFERENCE_H
+
+#include <string>
+using std::string;
+
+void generuoti_kryzmine_nuoroda(const string& ivestis_failas, const string& isvestis_failas);
+
+#endif //CROSS_REFERENCE_H
