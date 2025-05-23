c85dca2294916d665b69c9bf41bf9855b53dda0d
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-05-23 15:22:13 +0300

    .

---
+++ include/zodziu_skaiciavimas.h
@@ -0,0 +1,10 @@
+#ifndef ZODZIU_SKAICIAVIMAS_H
+#define ZODZIU_SKAICIAVIMAS_H
+
+#include <string>
+using std::string;
+
+void skaiciuoti_zodzius(const string& ivestis, const string& isvestis);
+string isvalyti_zodi(const string& zodis);
+
+#endif
